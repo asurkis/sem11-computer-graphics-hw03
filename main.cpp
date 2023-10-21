b800b539d9bc5a04ff759dc3a00422b1a74aef31
Anton Surkis <antonsurkis@gmail.com>
2023-10-21 14:03:10 +0300

    Refactor code

--- main.cpp
+++ main.cpp
@@ -9,66 +9,84 @@
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 
+namespace {
+
 void errorCallback(int error, const char *description) {
-  std::cerr << "GLFW error " << error << ": " << description << std::endl;
+    std::cerr << "GLFW error " << error << ": " << description << std::endl;
 }
 
-int main() {
-  glfwSetErrorCallback(errorCallback);
+GLFWwindow *window;
 
-  if (!glfwInit()) {
-    throw std::runtime_error("Could not initialize GLFW");
-  }
+void initContext() {
+    glfwSetErrorCallback(errorCallback);
 
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    if (!glfwInit()) {
+        throw std::runtime_error("Could not initialize GLFW");
+    }
 
-  GLFWwindow *window =
-      glfwCreateWindow(1280, 720, "Main window", nullptr, nullptr);
-  if (!window) {
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(1280, 720, "Main window", nullptr, nullptr);
+    if (!window) {
+        throw std::runtime_error("Could not create window");
+    }
+
+    glfwMakeContextCurrent(window);
+    gladLoadGL(glfwGetProcAddress);
+    glfwSwapInterval(0);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init();
+}
+
+void deinitContext() {
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
     glfwTerminate();
-    throw std::runtime_error("Could not create window");
-  }
+}
 
-  glfwMakeContextCurrent(window);
-  gladLoadGL(glfwGetProcAddress);
-  glfwSwapInterval(0);
-
-  IMGUI_CHECKVERSION();
-  ImGui::CreateContext();
-  ImGuiIO &io = ImGui::GetIO();
-  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-  io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
-
-  ImGui_ImplGlfw_InitForOpenGL(window, true);
-  ImGui_ImplOpenGL3_Init();
-
-  while (!glfwWindowShouldClose(window)) {
+void frameBegin() {
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
     ImGui::ShowDemoWindow();
+}
 
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-
+void frameEnd() {
     ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
     glfwSwapBuffers(window);
+
     glfwPollEvents();
-
     if (ImGui::IsKeyPressed(ImGuiKey::ImGuiKey_Escape)) {
-      glfwSetWindowShouldClose(window, GLFW_TRUE);
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
     }
-  }
+}
 
-  ImGui_ImplOpenGL3_Shutdown();
-  ImGui_ImplGlfw_Shutdown();
-  ImGui::DestroyContext();
+} // namespace
 
-  glfwDestroyWindow(window);
-  glfwTerminate();
-  return 0;
+int main() {
+    initContext();
+
+    while (!glfwWindowShouldClose(window)) {
+        frameBegin();
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        frameEnd();
+    }
+
+    deinitContext();
+    return 0;
 }
